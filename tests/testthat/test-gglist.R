4278355b130e2615204a826cfd70603763bf41cf
Bill Denney <wdenney@humanpredictions.com>
2024-06-05 10:36:01 -0400

    Allow addition of `guides()` objects

--- tests/testthat/test-gglist.R
+++ tests/testthat/test-gglist.R
@@ -21,6 +21,11 @@ test_that("vec_arith for gglists", {
     g1 + ggplot2::labs(x = "foo"),
     "gglist"
   )
+  # add a guides object to a single gglist
+  expect_s3_class(
+    g1 + ggplot2::guides(),
+    "gglist"
+  )
   # add a list of items to a single gglist
   expect_s3_class(
     g1 + list(ggplot2::labs(x = "foo")),
